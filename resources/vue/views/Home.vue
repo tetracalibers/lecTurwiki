a4af6b974ba0a19e69a6bf71be1aecdf22d3269f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-05-26 18:22:18 +0900

    devicon導入

--- resources/vue/views/Home.vue
+++ resources/vue/views/Home.vue
@@ -1,4 +1,12 @@
+<script setup>
+import icons from 'devicon/devicon.json'
+</script>
+
 <template>
 	<div>lecTurwik</div>
 	<div>概念から理解するプログラミング学習サイト</div>
+	
+	<div v-for="icon in icons" :key="icon.name">
+		<i :class="`devicon-${icon.name}-plain colored`"></i>
+	</div>
 </template>
