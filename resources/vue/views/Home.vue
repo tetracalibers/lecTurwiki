b02010a56467d8968d083583f29b807d90a78240
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-05-27 08:16:01 +0900

    仮トップページ作成

--- resources/vue/views/Home.vue
+++ resources/vue/views/Home.vue
@@ -5,7 +5,8 @@ import icons from 'devicon/devicon.json'
 <template>
 	<div>lecTurwik</div>
 	<div>概念から理解するプログラミング学習サイト</div>
-	
+	<div>コードの書き方ならいくらでも学べるこの時代。</div>
+	<div>そのコードの意味、本当にわかっていますか？</div>
 	<div v-for="icon in icons" :key="icon.name">
 		<i :class="`devicon-${icon.name}-plain colored`"></i>
 	</div>
