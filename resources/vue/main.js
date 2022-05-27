1cce49e1365055065d1193f43c11c3bc1bc2cca4
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-05-27 19:44:43 +0900

    Qui導入

--- resources/vue/main.js
+++ resources/vue/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import Qui from '@qvant/qui-max'
+import '@qvant/qui-max/styles'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(Qui).mount('#app')
